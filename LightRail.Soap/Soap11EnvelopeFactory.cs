18ed47db591c46226c9e19c57fc3e4909516544a
drakon <drakon660@gmail.com>
2023-06-14 16:51:19 +0200

    continue...

--- LightRail.Soap/Soap11EnvelopeFactory.cs
+++ LightRail.Soap/Soap11EnvelopeFactory.cs
@@ -6,29 +6,24 @@ namespace LightRail.Soap;
 
 public class Soap11EnvelopeFactory : ISoapEnvelopeFactory
 {
-    private readonly SoapMessageConfiguration _soapMessageConfiguration;
     private const string Schema = "http://schemas.xmlsoap.org/soap/envelope/";
     private const string MediaType = "text/xml";
-
-    public Soap11EnvelopeFactory()
-    {
-        _soapMessageConfiguration = new SoapMessageConfiguration(SoapVersion.Soap11);
-    }
+    private static readonly XNamespace XSchema = Schema;
 
     public StringContent Create(IEnumerable<XElement> headers, IEnumerable<XElement> bodies, string action)
     {
         XElement envelope = new
             XElement(
-                _soapMessageConfiguration.Schema + "Envelope",
+                XSchema + "Envelope",
                 new XAttribute(
                     XNamespace.Xmlns + "soapenv",
-                    _soapMessageConfiguration.Schema.NamespaceName));
+                    XSchema.NamespaceName));
         
         
         if (headers != null && headers.Any())
-            envelope.Add(new XElement(_soapMessageConfiguration.Schema + "Header", headers));
+            envelope.Add(new XElement(XSchema + "Header", headers));
 
-        envelope.Add(new XElement(_soapMessageConfiguration.Schema + "Body", bodies));
+        envelope.Add(new XElement(XSchema + "Body", bodies));
 
         var content = new StringContent(envelope.ToString(), Encoding.UTF8, MediaType);
         
@@ -36,9 +31,9 @@ public class Soap11EnvelopeFactory : ISoapEnvelopeFactory
         {
             content.Headers.Add("SOAPAction", action);
 
-            if (_soapMessageConfiguration.SoapVersion == SoapVersion.Soap12)
-                content.Headers.ContentType!.Parameters.Add(
-                    new NameValueHeaderValue("ActionParameter", $"\"{action}\""));
+            // if (_soapMessageConfiguration.SoapVersion == SoapVersion.Soap12)
+            //     content.Headers.ContentType!.Parameters.Add(
+            //         new NameValueHeaderValue("ActionParameter", $"\"{action}\""));
         }
 
         return content;
